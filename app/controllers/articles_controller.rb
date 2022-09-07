794dee79ae851739ea9a28afcea9cf0d25fec5fe
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-07 15:28:05 +0000

    [8]記事一覧を取り出す

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,22 @@
 class ArticlesController < ApplicationController
+def show
+  @article = Article.find(params[:id])
+end
+
 def new
 end
+
+def create
+  # render plain: params[:article].inspect
+  # raise params.inspect
+  @article = Article.new(article_params)# params.require(:article).permit(:title, :text)
+  @article.save
+  redirect_to article_path(@article)
+end
+
+
+def article_params
+  params.require(:article).permit(:title, :text)
+end
+
 end
