a3d25e2fbb684c32ea08d54d954b1b8484b82291
EC2 Default User <ec2-user@ip-172-31-27-68.ap-northeast-1.compute.internal>
2022-09-08 13:40:46 +0000

    Finish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,51 @@
 class ArticlesController < ApplicationController
+  before_action :set_article,only: [:show, :edit, :update, :destroy]
+
+def index
+  @articles = Article.all
+end
+
 def show
-  @article = Article.find(params[:id])
+end
+
+def edit
 end
 
 def new
+  @article = Article.new
+end
+
+def update
+  if @article.update(article_params)
+   redirect_to @article
+  else
+   render 'edit'
+  end
+end
+
+def destroy
+  @article.destroy
+  redirect_to articles_path
 end
 
 def create
   # render plain: params[:article].inspect
   # raise params.inspect
   @article = Article.new(article_params)# params.require(:article).permit(:title, :text)
-  @article.save
-  redirect_to article_path(@article)
+  if @article.save
+   redirect_to article_path(@article)
+  else
+   render 'new'
+  end
 end
 
 
 def article_params
   params.require(:article).permit(:title, :text)
+end
+
+def set_article
+  @article = Article.find(params[:id])
 end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,3 @@
 class Article < ApplicationRecord
+  validates :title,presence: true,length: { minimum: 5 }#titleが存在するか、文字数は5文字以上か
 end
